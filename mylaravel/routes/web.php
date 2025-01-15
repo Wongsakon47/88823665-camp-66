cada98a5e8e4cdb423de1d5528241a75e59b7b2a
Granetta <140387700+Wongsakon47@users.noreply.github.com>
2025-01-15 16:30:37 +0700

    Update web.php

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -6,3 +6,12 @@ use App\Http\Controllers\MyController;
 // กำหนดเส้นทางสำหรับการแสดงแบบฟอร์มและประมวลผลการส่ง
 Route::get('/mycontroller', [MyController::class, 'myfunction']);
 Route::post('/mycontroller', [MyController::class, 'myfunction']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello/{id?}',
+    function($var1="") {
+    return "<h1>Hello World $var1</h1>";
+});
